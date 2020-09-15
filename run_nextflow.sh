03b4466bdbf2ab414e325bc87b6e4f597728a850
AWS ParallelCluster user <centos@ip-10-0-0-133.us-west-1.compute.internal>
2020-09-15 05:11:47 +0000

    parallel-cluster version

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -3,8 +3,8 @@
 #State the custom file working on
 echo "Working on $1"
 
-#Load Singularity
-module load singularity
-
 #Run nextflow
-nextflow run -with-dag dag.png ./main.nf -c $1 -resume -bg > .information
+nextflow run -with-dag dag.png ./main.nf \
+         -c $1 \
+         -w /vol1/work/ \
+         -resume -bg > .information
